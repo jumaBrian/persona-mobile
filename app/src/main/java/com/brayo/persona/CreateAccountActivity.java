b8ef7bba4ae255af9871239ffae79854e2ccd5e6
jumaBrian <njengabrianj@gmail.com>
2022-06-16 23:11:59 +0300

    ft: Create User Object

--- app/src/main/java/com/brayo/persona/CreateAccountActivity.java
+++ app/src/main/java/com/brayo/persona/CreateAccountActivity.java
@@ -1,15 +1,32 @@
 package com.brayo.persona;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.brayo.persona.databinding.ActivityCreateAccountBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateAccountActivity extends AppCompatActivity {
+    // Firestore connection
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private ActivityCreateAccountBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +35,73 @@ public class CreateAccountActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         binding.accountCreateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(CreateAccountActivity.this,LoginActivity.class));
+
+                createUserEmailAccount(email, password, username);
+                startActivity(new Intent(CreateAccountActivity.this, LoginActivity.class));
             }
         });
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+
+                if (currentUser != null) {
+                    // user is already logged in
+                } else {
+                    // no user yet
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    private void createUserEmailAccount(String email, String password, String username) {
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(username)) {
+            binding.createAccountProgress.setVisibility(View.VISIBLE);
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // take user to AddJournal Activity
+                                currentUser = firebaseAuth.getCurrentUser();
+                                assert currentUser != null;
+                                String currentUserId = currentUser.getUid();
+
+                                // Create a user map so we can create a user in the user collection
+                                Map<String, String> userObj = new HashMap<>();
+                                userObj.put("userId", currentUserId);
+                                userObj.put("username", username);
+
+                                // Save to our firestore database
+                            } else {
+                                // something went wrong
+
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+
+
+        }
     }
 }
