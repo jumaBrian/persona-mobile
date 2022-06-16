e4ba7f426d7900d4a05dc095a2649a2065bb0ee0
jumaBrian <njengabrianj@gmail.com>
2022-06-16 18:03:49 +0300

    ft: Add second screen - Log In

--- app/src/main/java/com/brayo/persona/LauncherActivity.java
+++ app/src/main/java/com/brayo/persona/LauncherActivity.java
@@ -2,13 +2,29 @@ package com.brayo.persona;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.brayo.persona.databinding.ActivityLauncherBinding;
 
 public class LauncherActivity extends AppCompatActivity {
+    private ActivityLauncherBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
+        binding = ActivityLauncherBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LauncherActivity.this,LoginActivity.class));
+            }
+        });
+
+
     }
 }
